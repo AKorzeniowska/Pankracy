2e8a477822e5513be5e8309e1335df0f94e75546
Oskar <pawicao@gmail.com>
2020-05-20 15:03:46 +0200

    Getting weather information based on user's location

--- app/src/main/java/com/agh/edu/pankracy/WeatherTestActivity.java
+++ app/src/main/java/com/agh/edu/pankracy/WeatherTestActivity.java
@@ -1,21 +1,49 @@
 package com.agh.edu.pankracy;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
 import com.agh.edu.pankracy.data.weather.WeatherCollection;
 import com.agh.edu.pankracy.utils.JSONUtils;
 import com.agh.edu.pankracy.utils.NetworkUtils;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 
+import android.os.Looper;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.net.URL;
 
 public class WeatherTestActivity extends AppCompatActivity {
+    private static final String LOG_TAG = WeatherTestActivity.class.getSimpleName();
+
+    private int PERMISSION_ID = 44;
+    FusedLocationProviderClient mFusedLocationClient;
+    private double LATITUDE;
+    private double LONGITUDE;
+
     private TextView weatherTextView;
 
     @Override
@@ -33,20 +61,24 @@ public class WeatherTestActivity extends AppCompatActivity {
                 loadWeatherData();
             }
         });
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
     }
 
-
-    // Weather API Implementation
+    // -- Weather API Implementation --
     private void loadWeatherData() {
-        // TODO: Add getting localization
-        new FetchWeatherTask().execute(49.817225, 19.222505);
+        if (checkPermissions()) {
+            getLastLocation();
+        }
+        new FetchWeatherTask().execute(LATITUDE, LONGITUDE);
     }
 
+    @SuppressLint("StaticFieldLeak")
     public class FetchWeatherTask extends AsyncTask<Double, Void, WeatherCollection> {
 
         @Override
         protected WeatherCollection doInBackground(Double... params) {
-            if(params[0] == null || params[1] == null)
+            if (params[0] == null || params[1] == null)
                 return null;
             URL url = NetworkUtils.buildUrl(
                     params[0],
@@ -63,9 +95,100 @@ public class WeatherTestActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(WeatherCollection weatherCollection) {
-            if(weatherCollection != null)
+            if (weatherCollection != null)
                 weatherTextView.setText(weatherCollection.toString());
         }
 
     }
+
+    // -- Getting Location --
+
+    private void assignLocation(double latitude, double longitude) {
+        Log.v(LOG_TAG, "Latitude: " + latitude);
+        Log.v(LOG_TAG, "Longitude: " + longitude);
+        LATITUDE = latitude;
+        LONGITUDE = longitude;
+    }
+
+    private boolean checkPermissions() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(
+                this,
+                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
+                PERMISSION_ID
+        );
+    }
+
+    private boolean isLocationEnabled() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+                LocationManager.NETWORK_PROVIDER
+        );
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_ID) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLocation();
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getLastLocation() {
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+                mFusedLocationClient.getLastLocation().addOnCompleteListener(
+                        new OnCompleteListener<Location>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Location> task) {
+                                Location location = task.getResult();
+                                if (location == null) {
+                                    requestNewLocationData();
+                                } else {
+                                    assignLocation(location.getLatitude(), location.getLongitude());
+                                }
+                            }
+                        }
+                );
+            } else {
+                Toast.makeText(this, "Turn on location", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        } else {
+            requestPermissions();
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void requestNewLocationData() {
+
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(0);
+        mLocationRequest.setFastestInterval(0);
+        mLocationRequest.setNumUpdates(1);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mFusedLocationClient.requestLocationUpdates(
+                mLocationRequest, mLocationCallback,
+                Looper.myLooper()
+        );
+
+    }
+
+    private LocationCallback mLocationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            Location mLastLocation = locationResult.getLastLocation();
+            assignLocation(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+        }
+    };
 }
